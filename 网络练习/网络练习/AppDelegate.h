d99c18b651c61e97a417d5a816dc7315e999d093
qjx <qjx>
2016-03-12 19:54:46 +0800

    初始化项目

---
+++ 网络练习/网络练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网络练习
+//
+//  Created by 权建星 on 16/3/12.
+//  Copyright © 2016年 权建星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
